8e4d5b7b206be5f6e90f9889411d90f468b190d1
cau4alves <cau4alves@gmail.com>
2026-06-27 20:50:08 -0300

    feat(project): add post

--- src/main/java/br/com/caua/spring_boot_project/controller/HelloController.java
+++ src/main/java/br/com/caua/spring_boot_project/controller/HelloController.java
@@ -3,28 +3,21 @@ package br.com.caua.spring_boot_project.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/hello")
 public class HelloController {
 
-//    @GetMapping
-//    public String hello() {
-//        return "hello";
-//    }
-
-//    @GetMapping
-//    public ResponseEntity<String> hello() {
-//        return ResponseEntity.ok("hello");
-//    }
-
     @GetMapping
-    public ResponseEntity<String> hello() {
-        return new ResponseEntity<>("hello", HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public String helloGet() {
+        return "hello Get";
     }
 
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String helloPost() {
+        return "hello Post";
+    }
 }
